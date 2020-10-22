ffbbb15b7ebae373ef5bef66114af8037e6fcdca
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-22 12:38:39 +0200

    Function to initialize Points, pass by reference example

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <array>
+#include <cstdlib>
 #include <iostream>
 
 // Define a struct ---> NEW TYPE
@@ -7,17 +7,40 @@ struct Point2D {
     double y;
 };
 
+// INPUT ---> {????} ---> OUTPUT
+//     {initialize struct}
+
+Point2D create_point() {
+    Point2D tmp;
+    tmp.x = rand() % 10;  
+    tmp.y = rand() % 10; 
+
+    return tmp;
+}
+
+void initialize_in_place(Point2D& point) {
+    point.x = rand() % 10;  
+    point.y = rand() % 10; 
+
+    std::cout << "Value of X: " << point.x << std::endl;
+    std::cout << "Value of Y: " << point.y << std::endl;
+}
+
 int main() {
     // Declare a variable with our struct
-    Point2D mypoint;
-
-    // Initialize its members
-    mypoint.x = 3.3;
-    mypoint.y = 5.5;
+    Point2D mypoint = create_point();
 
     // Print its contents
-    std::cout << "Value of X: " << mypoint.x << std::endl;
-    std::cout << "Value of Y: " << mypoint.y << std::endl;
+    // std::cout << "Value of X: " << mypoint.x << std::endl;
+    // std::cout << "Value of Y: " << mypoint.y << std::endl;
+
+    // Declare a variable with our struct
+    Point2D empty_point;
+    initialize_in_place(empty_point);
+
+    // Print its contents
+    std::cout << "Value of X: " << empty_point.x << std::endl;
+    std::cout << "Value of Y: " << empty_point.y << std::endl;
 
     return 0;
 }
