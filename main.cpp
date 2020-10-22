84db1cb57989968dad4741a237ce54dcff5a291e
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-22 15:04:33 +0200

    Matrix addion

--- main.cpp
+++ main.cpp
@@ -5,6 +5,52 @@
 struct Matrix2D {
     std::vector<std::vector<int>> contents;
 
+    void add(Matrix2D other) {
+        if (other.rows() == rows() && other.columns() == columns()) {
+
+            for (int i = 0; i < rows(); i++){
+                for (int j = 0; j < columns(); j++) {
+                    contents[i][j] += other.contents[i][j];
+                }
+            }  
+
+        } else {
+            std::cout << "Dimensions must match!" << std::endl;
+        }
+
+    }
+
+    int rows() {
+        return contents.size();
+    }
+
+    int columns() {
+        return contents[0].size();
+    }
+
+    void input_matrix() {
+        int columns, rows;
+
+        std::cout << "Number of rows: "<< std::endl;
+        std::cin >> rows;
+
+        std::cout << "Number of columns: "<< std::endl;
+        std::cin >> columns;
+
+        for (int i = 0; i < rows; i++){
+            std::vector<int> row;
+            int tmp;
+            std::cout << "Adding a row with " << columns << " columns" << std::endl;
+            for (int j = 0; j < columns; j++) {
+                std::cout << "Introduce value: "<< std::endl;
+                std::cin >> tmp;
+                row.push_back(tmp);
+            }
+
+            contents.push_back(row);
+        }
+    }
+
     void print() {
         std::cout << "Contents of the matrix:" << std::endl;
 
@@ -21,26 +67,24 @@ struct Matrix2D {
 };
 
 int main() {
-    // A 3X3, B 3x3
+    Matrix2D a, b;
 
-    // Declare the matrices
-    Matrix2D a;
-    Matrix2D b;
+    std::cout << "Introduce data for matrix a:" << std::endl;
 
-    // Initialize the contents
-    a.contents = {{1,2,3}, {4,5,6}, {7,8,9}};
-    b.contents = {{9,8,7}, {6,5,4}, {3,2,1}};
+    a.input_matrix();
 
-    // A = [ 1, 2, 3
-    //       4, 5, 6
-    //       7, 8, 9]
+    std::cout << "Introduce data for matrix b:" << std::endl;
 
-    // B = [ 9, 8, 7
-    //       6, 5, 4
-    //       3, 2, 1]
+    b.input_matrix();
 
+    std::cout << "Print matrix a:" << std::endl;
     a.print();
+    std::cout << "Print matrix b:" << std::endl;
     b.print();
+
+    std::cout << "Adding b to a:" << std::endl;
+    a.add(b);
+    a.print();
 
     return 0;
 }
