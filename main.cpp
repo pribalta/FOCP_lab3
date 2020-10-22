d1fcc99d08e7af1dc4bc7bc9e17f3d10eeb7ee01
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-22 12:23:58 +0200

    Initial skeleton for struct

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include <array>
+#include <iostream>
+
+// Define a struct ---> NEW TYPE
+struct Point2D {
+    double x;
+    double y;
+};
+
 int main() {
+    // Declare a variable with our struct
+    Point2D mypoint;
+
+    // Initialize its members
+    mypoint.x = 3.3;
+    mypoint.y = 5.5;
+
+    // Print its contents
+    std::cout << "Value of X: " << mypoint.x << std::endl;
+    std::cout << "Value of Y: " << mypoint.y << std::endl;
+
     return 0;
 }
